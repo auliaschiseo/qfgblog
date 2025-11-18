8186cf6f740843bfdf98c4daf2dd96e0896f279a
auliaschiseo <aulia.work9@gmail.com>
2025-11-18 15:22:29 +0700

    Fix adapter config - use object instead of string

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
