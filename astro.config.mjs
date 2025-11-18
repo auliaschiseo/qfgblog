60ed6af6fd5035cc957102290aa4cea5662f65c5
auliaschiseo <aulia.work9@gmail.com>
2025-11-18 15:01:55 +0700

    Add config for Cloudflare Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
+    adapter: '@astrojs/cloudflare',
     integrations: [mdx(), sitemap(), tailwind()],
     markdown: {
         shikiConfig: {
